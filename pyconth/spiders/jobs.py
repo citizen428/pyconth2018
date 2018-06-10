eb023f3fee6fb847eefdd8b1f7eeca2015cdee03
Michael Kohl <citizen428@gmail.com>
2018-06-10 16:33:35 +0700

    Create simple spider

--- pyconth/spiders/jobs.py
+++ pyconth/spiders/jobs.py
@@ -7,5 +7,19 @@ class JobsSpider(scrapy.Spider):
     allowed_domains = ['www.python.org']
     start_urls = ['https://www.python.org/jobs/']
 
+    # Find job pages
     def parse(self, response):
-        pass
+        for jobs in response.css('.pagination a[href^="?page"]::attr(href)').extract():
+            page = response.urljoin(jobs)
+            yield scrapy.Request(page, callback=self.parse_jobs)
+
+    # Extract jobs from job pages
+    def parse_jobs(self, response):
+        for job in response.css('.list-recent-jobs > li'):
+            yield {
+                "title": job.css('.listing-company-name > a::text').extract_first(),
+                "company": job.css('.listing-company-name::text').extract(),
+                "location": job.css('.listing-location > a::text').extract_first(),
+                "tags": job.css('.listing-job-type > a::text').extract(),
+                "date": job.css('.listing-posted > time::text').extract_first()
+            }
